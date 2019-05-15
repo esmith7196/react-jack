7efa7080290bb9bfa549715e11e880e3a6911ad4
Eric Smith <ericsmith.100@gmail.com>
2019-05-15 18:56:56 -0400

    connect to deckofcardsapi with axio

--- react-jack/src/components/Table.js
+++ react-jack/src/components/Table.js
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Playerhand from "./Playerhand";
-import Dealerhand from "./Dealerhand";
+import axios from "axios";
 
 const Table = props => {
+  // Make a request to get n number of decks
+  const [deckId, getDeckId] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=2`)
+      .then(res => {
+        getDeckId(res.data.deck_id);
+        console.log(res.data);
+      });
+  }, []);
+
   return (
     <div style={{ width: "100%" }}>
       <p>this is the table</p>
@@ -10,6 +22,10 @@ const Table = props => {
         <Playerhand />
 
         <p>Cards remaining: </p>
+        <h2>Deck Info</h2>
+        <ul>
+          <li>The deck id is: {deckId}</li>
+        </ul>
       </div>
     </div>
   );
