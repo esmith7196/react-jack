b6836616f50fc1ed5b4b9cbd833d1622c049770a
Eric Smith <ericsmith.100@gmail.com>
2019-05-18 08:55:50 -0400

    Parse values as int / handle face cards (not ace yet)

--- react-jack/src/components/Playerhand.js
+++ react-jack/src/components/Playerhand.js
@@ -4,7 +4,12 @@ const Playerhand = props => {
   return (
     <div>
       <button onClick={props.deal}>Deal</button>
-      <p>Player cards are</p>
+      <p>Player cards are:</p>
+      <div className="card-container">
+        {props.cards.map(card => (
+          <img src={card.image} alt={card.value} />
+        ))}
+      </div>
     </div>
   );
 };
--- react-jack/src/components/Table.js
+++ react-jack/src/components/Table.js
@@ -5,7 +5,8 @@ import axios from "axios";
 const Table = props => {
   // Make a request to get n number of decks
   const [deckId, getDeckId] = useState(null);
-  const [cards, getCards] = useState([]);
+  const [cards, setCards] = useState([]);
+  const [playerSum, setPlayerSum] = useState([0]);
 
   useEffect(() => {
     axios
@@ -21,24 +22,49 @@ const Table = props => {
       .get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
       .then(res => {
         console.log("deck response is: ", res);
-        getCards(res.data.cards);
+        setCards(res.data.cards);
       });
   }, [deckId]);
+
+  useEffect(() => {
+    let valueArr = [];
+
+    cards.map(card => {
+      if (
+        card.value === "KING" ||
+        card.value === "QUEEN" ||
+        card.value === "JACK"
+      ) {
+        card.value = 10;
+      } else if (card.value === "ACE") {
+        alert("handle ace case");
+        card.value = 11;
+      }
+      valueArr.push(parseInt(card.value));
+    });
+    console.log(valueArr);
+    sumCards(valueArr);
+
+    function sumCards(item) {
+      setPlayerSum(item.reduce(getSum, 0));
+    }
+  }, [cards]);
+
+  console.log("cards is: ", cards);
+  function getSum(total, num) {
+    return total + num;
+  }
+
+  console.log(playerSum);
 
   return (
     <div style={{ width: "100%" }}>
       <p>this is the table</p>
       <div className="">
-        <Playerhand />
+        <Playerhand cards={cards} />
 
         <p>Cards remaining: </p>
         <h2>Deck Info</h2>
-        <ul>
-          <li>The deck id is: {deckId}</li>
-          {cards.map(card => (
-            <p>{card.value}</p>
-          ))}
-        </ul>
       </div>
     </div>
   );
